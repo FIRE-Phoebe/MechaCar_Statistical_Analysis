6513729dbe19627a213a52d186ba465e8630f22c
FIRE-Phoebe <karminabrenna@gmail.com>
2022-04-07 15:33:33 -0700

    'add_imgs'

--- Resources/data/MechaCarChallenge.R
+++ Resources/data/MechaCarChallenge.R
@@ -21,8 +21,19 @@ total_summary <- summarize(table_tech, Mean=mean(PSI),Median= median(PSI),Varian
 lot_summary <- table_tech%>%group_by(Manufacturing_Lot)%>%summarize(Mean=mean(PSI),Median= median(PSI),Variance=var(PSI),SD= sd(PSI),.groups = 'keep')
 
 
+#use t.test() to determine if the PSI across all manuf. lots is 
+#statistically diff from pop.mean of 1,500 pounds per sq. inch.
+t.test(table_tech$PSI, mu=1500)
 
-
+#t.test() for each individual lots, and subset() argument determine
+#if the PSI for each manufacturing lot is statistically different from 
+#the population mean of 1,500 pounds per square inch.
+Lot1_table <- subset(table_tech, Manufacturing_Lot == 'Lot1')
+Lot2_table <- subset(table_tech, Manufacturing_Lot == 'Lot2')
+Lot3_table <- subset(table_tech, Manufacturing_Lot == 'Lot3')
+t.test(Lot1_table$PSI, mu=1500)
+t.test(Lot2_table$PSI, mu=1500)
+t.test(Lot3_table$PSI, mu=1500)
 
 
 
